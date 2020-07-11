6c3c4350fd72850791fb08fbb250cef3eeb76865
yangtaihua <957651480@qq.com>
2020-07-12 04:23:18 +0800

    代码修改

--- app/Http/Controllers/Api/Backend/BannerController.php
+++ app/Http/Controllers/Api/Backend/BannerController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Api\Backend;
 use App\Http\Response\ApiResponse;
 use App\Models\Banner;
 use App\Services\FileService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class BannerController extends BackendApiController
@@ -21,16 +22,57 @@ class BannerController extends BackendApiController
 
     public function create(Request $request)
     {
-        Banner::create($request->all());
+        $data = $this->validateCreateBanner($request);
+        Banner::create($data);
+        return api_response()->success();
    }
 
-
-    protected function validateBanner($form)
+    public function detail(Request $request,int $id)
     {
-        $validator = validator($form);
-        if($validator->fails()){
-            return api_response()->fail(['msg'=>$validator->messages()->first()]);
-        }
-        return $validator->validated();
+        $banner = $this->firstBannerByIdOrFail($id);
+        return api_response()->success(['data'=>$banner]);
+   }
+
+    public function update(Request $request,int $id)
+    {
+        $banner = $this->firstBannerByIdOrFail($id);
+        $banner->setRawAttributes($request->all());
+        return api_response()->success();
+   }
+
+    public function delete(Request $request, int $id)
+    {
+        $banner = $this->firstBannerByIdOrFail($id);
+        $banner->delete();
+        return api_response()->success();
+   }
+
+    /**
+     * @param int $id
+     * @return Banner|Banner[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     */
+    protected function firstBannerById(int $id)
+    {
+        return Banner::find($id);
+   }
+
+    /**
+     * @param int $id
+     * @return Banner|Banner[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     * @throws \Throwable
+     */
+    protected function firstBannerByIdOrFail(int $id)
+    {
+        $banner = $this->firstBannerById($id);
+        throw_unless($banner,ModelNotFoundException::class);
+        return $banner;
+    }
+    protected function validateCreateBanner(Request $request)
+    {
+        return $request->validate([
+            'title'=>'required'
+        ],[
+            'title.required'=>'标题必须'
+        ]);
    }
 }
