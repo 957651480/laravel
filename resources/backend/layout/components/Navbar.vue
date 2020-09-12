52f2b8818268ad4abd077121cbc944c9bc31873f
yangtaihua <957651480@qq.com>
2020-09-13 01:35:22 +0800

    汇率

--- resources/backend/layout/components/Navbar.vue
+++ resources/backend/layout/components/Navbar.vue
@@ -25,14 +25,14 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
-          <router-link to="/profile/index">
+          <!--<router-link to="/profile/index">
             <el-dropdown-item>Profile</el-dropdown-item>
-          </router-link>
+          </router-link>-->
           <router-link to="/">
-            <el-dropdown-item>Dashboard</el-dropdown-item>
+            <el-dropdown-item>仪表板</el-dropdown-item>
           </router-link>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">退出</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
