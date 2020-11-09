6eb43e4851bf1cff8b3a3a6c893d67ff307d3d5d
yangtaihua <957651480@qq.com>
2020-11-09 21:30:47 +0800

    dfgfd

--- database/migrations/2020_04_29_043802_create_sys_region_table.php
+++ database/migrations/2020_04_29_043802_create_sys_region_table.php
@@ -15,17 +15,10 @@ class CreateSysRegionTable extends Migration
     {
         Schema::create('sys_region', function (Blueprint $table) {
             $table->integerIncrements('id')->comment('地区id');
-            $table->string('short_name')->default('')->comment('短名称');
             $table->string('name')->default('')->index()->comment('名称');
-            $table->string('merger_name')->default('')->comment('合并名称');
             $table->integer('parent_id')->default(0)->index()->comment('父id');
             $table->integer('level')->default(1)->comment('层级');
-            $table->string('pinyin')->default('')->comment('拼音');
-            $table->string('code')->default('')->comment('长途区号');
-            $table->string('zip_code')->default('')->comment('邮编');
-            $table->string('first')->default('')->comment('首字母');
-            $table->string('lng')->default('')->comment('经度');
-            $table->string('lat')->default('')->comment('纬度');
+            $table->string('path')->default('')->comment('path');
             $table->softDeletes();
             $table->timestamps();
         });
