5e7f34050f28638302c4a5ddffbf2c5fa2dc02cd
yangtaihua <957651480@qq.com>
2020-11-09 22:19:49 +0800

    dfgfd

---
+++ app/Http/Controllers/SysTreeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sys\SysTree;
+use Illuminate\Http\Request;
+
+class SysTreeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sys\SysTree  $sysTree
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SysTree $sysTree)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sys\SysTree  $sysTree
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SysTree $sysTree)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sys\SysTree  $sysTree
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SysTree $sysTree)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sys\SysTree  $sysTree
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SysTree $sysTree)
+    {
+        //
+    }
+}
---
+++ database/factories/Sys/SysTreeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Sys;
+
+use App\Models\Sys\SysTree;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SysTreeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = SysTree::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
