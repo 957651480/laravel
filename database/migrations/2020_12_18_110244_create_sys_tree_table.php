adcebaed6de091634bbdee23d3b150a1a79c7da5
yangtaihua <957651480@qq.com>
2020-12-18 11:06:29 +0800

    sys_tree 结构表创建

---
+++ database/migrations/2020_12_18_110244_create_sys_tree_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSysTreeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sys_tree', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->comment('标识');
+            $table->string('name')->comment('名称');
+            $table->string('value')->comment('值');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sys_tree');
+    }
+}
