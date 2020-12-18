4d34df6715d1cdd5b55840c8aa0de1b1bf7ab522
yangtaihua <957651480@qq.com>
2020-12-18 22:07:24 +0800

    00

--- app/Models/Sys/SysTree.php
+++ app/Models/Sys/SysTree.php
@@ -5,7 +5,7 @@ namespace App\Models\Sys;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tree extends Model
+class SysTree extends Model
 {
     use HasFactory;
 }
---
+++ database/seeders/SysTreeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Sys\SysTree;
+use Illuminate\Database\Seeder;
+
+class SysTreeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        SysTree::create();
+    }
+}
