d13ab2120f5a61f7b17e7d20045dbc240b4edeb9
yangtaihua <957651480@users.noreply.github.com>
2021-03-16 15:06:34 +0800

    inertia-react 集成

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Inertia\Inertia;
+
+class TestController extends Controller
+{
+
+    public function test()
+    {
+        return  Inertia::render('Test/Test');
+    }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,25 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
+mix.extract();
+mix.js('resources/js/app.js', 'public/js').react()
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ])
+    .webpackConfig(require('./webpack.config'));
+
+if (mix.inProduction()) {
+    mix.version();
+} else {
+
+        //mix.eslint();
+
+    // Development settings
+    mix
+        .sourceMaps()
+        .webpackConfig({
+            devtool: 'eval-source-map', // Fastest for development
+        });
+}
