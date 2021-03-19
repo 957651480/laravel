99928bc2e3b88a4c1e1fe8daa6def85b31f02acb
yangtaihua <957651480@users.noreply.github.com>
2021-03-19 16:43:24 +0800

    ant-design-pro 修改

--- resources/js/Pages/Test/AntProTest.tsx
+++ resources/js/Pages/Test/AntProTest.tsx
@@ -1,34 +1,24 @@
-import React, { useState } from 'react'
-import ProLayout, {PageContainer,SettingDrawer} from '@ant-design/pro-layout';
-import type { ProSettings } from '@ant-design/pro-layout';
+import React from 'react';
+import { InertiaLink, usePage } from '@inertiajs/inertia-react';
+// @ts-ignore
+import Layout from '@/Pages/Layouts/Layout';
 
-export default ()=>{
-    const [settings, setSetting] = useState<Partial<ProSettings> | undefined>({ fixSiderbar: true });
-    return(
-        <div
-            id="test-pro-layout"
-            style={{
-                height: '100vh',
-            }}
-        >
-            <ProLayout
 
-                menuItemRender={
-                    false
-                }
+const Index = () => {
 
-            >
-                <PageContainer>
-                    00000
-                </PageContainer>
-            </ProLayout>
-            <SettingDrawer
-                pathname='/'
-                getContainer={() => document.getElementById('test-pro-layout')}
-                settings={settings}
-                onSettingChange={(changeSetting) => setSetting(changeSetting)}
-                disableUrlParams
-            />
+    // @ts-ignore
+    const { users } = usePage().props;
+    const {
+        data,
+        meta: { links }
+    } = users;
+    return (
+        <div>
+            0000
         </div>
-    )
-}
+    );
+};
+
+Index.layout = (page: any) => <Layout title="Users" children={page} />;
+
+export default Index;
