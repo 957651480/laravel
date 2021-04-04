88dc2a70b881185fc285e6b35e3910951e98f5f1
yangtaihua <957651480@qq.com>
2021-04-05 02:45:33 +0800

    添加功能

--- resources/js/Pages/File/Disk/Setting.tsx
+++ resources/js/Pages/File/Disk/Setting.tsx
@@ -1,9 +1,9 @@
 import React,{ useState } from 'react';
-import { message } from 'antd';
+import { message,Tabs } from 'antd';
 import ProForm, { ProFormText, ProFormDateRangePicker, ProFormSelect } from '@ant-design/pro-form';
 // @ts-ignore
 import Layout from '@/Pages/Layouts/Layout';
-
+const { TabPane } = Tabs;
 const waitTime = (time: number = 100) => {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -13,12 +13,10 @@ const waitTime = (time: number = 100) => {
 };
 const Setting = () => {
 
+
     return (
         <div
-            style={{
-                width: 330,
-                margin: 'auto',
-            }}
+
         >
             <ProForm<{
                 name: string;
@@ -34,53 +32,64 @@ const Setting = () => {
                     useMode: 'chapter',
                 }}
             >
-                <ProForm.Group>
-                    <ProFormText
-                        width="md"
-                        name="name"
-                        label="签约客户名称"
-                        tooltip="最长为 24 位"
-                        placeholder="请输入名称"
-                    />
-                    <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
-                </ProForm.Group>
-                <ProForm.Group>
-                    <ProFormText
-                        name={['contract', 'name']}
-                        width="md"
-                        label="合同名称"
-                        placeholder="请输入名称"
-                    />
-                    <ProFormDateRangePicker width="md" name={['contract', 'createTime']} label="合同生效时间" />
-                </ProForm.Group>
-                <ProForm.Group>
-                    <ProFormSelect
-                        options={[
-                            {
-                                value: 'chapter',
-                                label: '盖章后生效',
-                            },
-                        ]}
-                        readonly
-                        width="xs"
-                        name="useMode"
-                        label="合同约定生效方式"
-                    />
-                    <ProFormSelect
-                        width="xs"
-                        options={[
-                            {
-                                value: 'time',
-                                label: '履行完终止',
-                            },
-                        ]}
-                        name="unusedMode"
-                        label="合同约定失效效方式"
-                    />
-                </ProForm.Group>
-                <ProFormText width="sm" name="id" label="主合同编号" />
-                <ProFormText name="project" width="md" disabled label="项目名称" initialValue="xxxx项目" />
-                <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
+                <Tabs tabPosition={"left"}>
+                    <TabPane tab="站点配置" key="1">
+                        <ProForm.Group>
+                            <ProFormText
+                                width="md"
+                                name="name"
+                                label="签约客户名称"
+                                tooltip="最长为 24 位"
+                                placeholder="请输入名称"
+                            />
+                            <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
+                        </ProForm.Group>
+                        <ProForm.Group>
+                            <ProFormText
+                                name={['contract', 'name']}
+                                width="md"
+                                label="合同名称"
+                                placeholder="请输入名称"
+                            />
+                            <ProFormDateRangePicker width="md" name={['contract', 'createTime']} label="合同生效时间" />
+                        </ProForm.Group>
+                        <ProForm.Group>
+                            <ProFormSelect
+                                options={[
+                                    {
+                                        value: 'chapter',
+                                        label: '盖章后生效',
+                                    },
+                                ]}
+                                readonly
+                                width="xs"
+                                name="useMode"
+                                label="合同约定生效方式"
+                            />
+                            <ProFormSelect
+                                width="xs"
+                                options={[
+                                    {
+                                        value: 'time',
+                                        label: '履行完终止',
+                                    },
+                                ]}
+                                name="unusedMode"
+                                label="合同约定失效效方式"
+                            />
+                        </ProForm.Group>
+                        <ProFormText width="sm" name="id" label="主合同编号" />
+                        <ProFormText name="project" width="md" disabled label="项目名称" initialValue="xxxx项目" />
+                        <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
+                    </TabPane>
+                    <TabPane tab="磁盘配置" key="2">
+                        Content of Tab 2
+                    </TabPane>
+                    <TabPane tab="Tab 3" key="3">
+                        Content of Tab 3
+                    </TabPane>
+                </Tabs>
+
             </ProForm>
         </div>
 
