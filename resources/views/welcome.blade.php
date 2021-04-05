38edde924bf0e0538f8815e773cd94392983032b
yangtaihua <957651480@qq.com>
2021-04-05 20:58:38 +0800

    测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,5 +128,12 @@
                 </div>
             </div>
         </div>
+        <div id="example"></div>
     </body>
+    <!-- Scripts -->
+    <script src="/js/manifest.js"></script>
+    <script src="/js/vendor.js"></script>
+    <script src="/js/app.js"></script>
+<script>
+</script>
 </html>
