e1d71c7b390f212f68473f4efed7c41fd5d0dccc
yangtaihua <957651480@qq.com>
2021-04-21 22:09:27 +0800

    axios.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,11 @@ const report = rawArgv.includes('--report');
 let plugins = [
 
 ];
+plugins.push(['import', {
+    'libraryName': 'ant-design-vue',
+    'libraryDirectory': 'es',
+    'style': true // `style: true` 会加载 less 文件
+}])
 if (report) {
     plugins.push(new BundleAnalyzerPlugin({
         openAnalyzer: true,
