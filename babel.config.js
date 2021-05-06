88e7948848593c52e43a187f8ba72b078eaff6bb
yangtaihua <957651480@qq.com>
2021-05-06 20:04:01 +0800

    react 单页应用实现

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
+
+const plugins = ["@babel/plugin-transform-runtime"]
+plugins.push([
+    "import",
+    {
+        "libraryName": "antd",
+        "libraryDirectory": "es",
+        "style": "less"
+    }
+],)
+
+
+module.exports = {
+    presets: [
+        '@babel/preset-react',
+        [
+            '@babel/preset-env',
+            {
+                'useBuiltIns': 'entry',
+                'corejs': 3
+            }
+        ]
+    ],
+    plugins
+}
